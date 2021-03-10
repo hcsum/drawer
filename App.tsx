e61839e5f140df7ff5b5d0524b7419c18a43c85e
Sum <sum@Sums-MacBook-Pro.local>
2021-03-10 22:28:16 +0800

    color palette

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Home from "./components/Home";
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import ColorPalette from './screens/ColorPalette';
+import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Home />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-  },
-});
